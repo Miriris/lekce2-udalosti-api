7318ac87da808719911666e52b10f65a4a6eedca
Miriam <rezakmiriam@gmail.com>
2022-02-15 20:13:18 +0100

    Ukol1 - zmena pisma na cervene

--- main.js
+++ main.js
@@ -9,3 +9,25 @@ nadpis.classList.remove('zluty');
 
 let ctverecek = document.querySelector('div.zluty');
 ctverecek.classList.add('ctverecek');
+
+let pocitadlo = 0;
+function priNajetiNaCtverec() {
+    console.log('test' + pocitadlo);
+    pocitadlo++;
+}
+
+function zmenStyl() {
+    nadpis.classList.toggle('zeleny');
+}
+
+function ztucni() {
+    let pismo = document.querySelector('p.lev');
+    pismo.classList.toggle('tucne');
+}
+
+function zmenBarvuPisma() {
+    let barva = document.querySelector('p.lev');
+    barva.classList.add('cervenePismo');
+
+}
+
